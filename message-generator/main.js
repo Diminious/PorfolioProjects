a211d77797dcf8a93cd57fad89758fad7afc76ee
Diminious <matthewzengoski@gmail.com>
2024-04-24 19:09:11 +1000

    removes redundant comments

--- message-generator/main.js
+++ message-generator/main.js
@@ -1,45 +1,5 @@
-//TODO generate a random message every time code script is ran
-//TODO log to console
-// Truly randomness:
-//    output should be made up of at least three different pieces of data
 
-/* 
-    Hot Fuzz one liner bits
-
-    ?Past Tense
-    made people diappear
-    fired a gun
-    fired two guns
-    been in a high speed pursuit
-    seen Bad Boys II
-    been Judge Judy and executioner
-    had four aces beat a straight
-    taken a shortcut before
-    gone "Aaargh!"
-    had bolognese
-    ?Present Tense
-    needing to be stopped
-    jumping through the air
-    in a high speed pursuit
-    having a bad hand
-    going "Aaargh!"
-
-
-    Three sections
-    Have you ever
-    [
-        (done) first thing
-    ]
-    and
-    [
-        (done) second thing
-    ] 
-    whilst
-    [
-        (doing) something else
-    ]
-*/
-
+//all different phrases
 const past1 = "made people diappear"
 const past2 = "fired a gun"
 const past3 = "fired two guns"
@@ -60,6 +20,7 @@ const present5 = 'going "Aaargh!"'
 const pastPhrases = [past1, past2, past3, past4, past5, past6, past7, past8, past9, past10]
 const presentPhrases = [present1, present2, present3, present4, present5]
 
+//genreates random phrase from the array given
 function generatePhrase(phrases){
     const randNum = Math.floor(Math.random() * phrases.length)
     return phrases[randNum]
@@ -78,4 +39,4 @@ function generateRandomMessage(){
     return `Have you ever ${firstPhrase} and ${secondPhrase} whilst ${thirdPhrase}?`
 }
 
-console.log(generateRandomMessage())
+console.log("===\n" + generateRandomMessage() + "\n===")
